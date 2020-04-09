683ce7304a2e411731ec5ed9c32ef0357b76b99f
TREY <hugh.a.lloyd@gmail.com>
2020-04-09 16:27:49 -0500

    added env and script for deployment

---
+++ schemes/scheme-model.js
@@ -0,0 +1,46 @@
+const db = require("../data/dbConfig");
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes").where({ id });
+}
+
+function findSteps(id) {
+  return db("steps")
+    .join("schemes", "schemes.id", "=", "steps.scheme_id")
+    .where("schemes.id", "=", id)
+    .orderBy("steps.step_number");
+}
+
+async function add(scheme) {
+  const [id] = await db("schemes").insert(scheme);
+  return findById(id);
+}
+// function schemeFilter(value) {
+//     return db('schemes')
+//     .where
+// }
+
+function update(changes, id) {
+  return db("schemes")
+    .update(changes)
+    .where({ id });
+}
+
+function remove(id) {
+  return db("schemes")
+    .where({ id })
+    .del();
+}
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
